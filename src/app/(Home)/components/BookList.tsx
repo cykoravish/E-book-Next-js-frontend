a8cfb8071126bc74dbb2a300cdcbe6b4b3efef38
Ravish Bisht <ravishbisht85.com>
2024-11-10 11:08:19 +0530

    add books list

---
+++ src/app/(Home)/components/BookList.tsx
@@ -0,0 +1,18 @@
+import { Book } from "@/types";
+import React from "react";
+import BookCard from "./BookCard";
+
+const BookList = ({ books }: { books: Book[] }) => {
+  return (
+    <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 md:grid-cols-2 max-w-7xl mx-auto">
+      {books.map((book) => (
+        <div key={book._id}>
+          <h1>{book.title}</h1>
+          <BookCard book={book} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BookList;
--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -1,9 +1,17 @@
 import Banner from "@/app/(Home)/components/Banner";
+import BookList from "./components/BookList";
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch(`${process.env.BACKEND_URL}/books`);
+  if (!response.ok) {
+    throw new Error("An error occoured while fetching the books");
+  }
+  const books = await response.json();
+  console.log(books);
   return (
     <>
       <Banner />
+      <BookList books={books} />
     </>
   );
 }
