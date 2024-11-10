7a9f976b4112099227a05128c4468c382edf6845
Ravish Bisht <ravishbisht85.com>
2024-11-10 11:52:02 +0530

    add footer

--- src/app/(Home)/components/BookList.tsx
+++ src/app/(Home)/components/BookList.tsx
@@ -4,7 +4,7 @@ import BookCard from "./BookCard";
 
 const BookList = ({ books }: { books: Book[] }) => {
   return (
-    <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 md:grid-cols-2 max-w-7xl mx-auto">
+    <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 md:grid-cols-2 max-w-7xl mx-auto mb-10">
       {books.map((book) => (
         <div key={book._id}>
           <h1>{book.title}</h1>
